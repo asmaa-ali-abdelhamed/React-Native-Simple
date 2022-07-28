662ddd5a35da0419b4813de058be85bd45721f23
asmaa-ali-abdelhamed <92632307+asmaa-ali-abdelhamed@users.noreply.github.com>
2022-07-28 20:12:11 +0200

    Update

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet , Icons } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 // You can import from local files
 import AssetExample from './components/AssetExample';
 import Home from './components/Home';
-import Todo from './components/Todo/Todo';
-import AddTodo from './components/Todo/AddTodo';
+import Product from './components/Product';
+import Form from './components/Form';
+import RandomText from './components/RandomText';
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
@@ -17,10 +18,18 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <>
+     <>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="todo" component={Todo} options={{title: "Todo" }}/>        
+          <Tab.Screen name="Main Home" component={Home} options={{title: "Home Page" }}/>
+
+          <Tab.Screen name="Product" component={Product} 
+        options={{title: "Product Page"}}
+        />
+
+          <Tab.Screen name="form" component={Form} 
+        options={{title: "Form Page"}}
+        />
         </Tab.Navigator>
       </NavigationContainer>
     </>
@@ -28,5 +37,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  
 });
